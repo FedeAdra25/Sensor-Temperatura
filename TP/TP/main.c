c04efcc57258f4e2ab747dd73ef72852c6d6ec7a
FedeAdra25 <fedeadra@gmail.com>
2021-07-26 18:38:17 -0300

    ey

---
+++ TP/TP/main.c
@@ -0,0 +1,18 @@
+/*
+ * TP.c
+ *
+ * Created: 26-Jul-21 6:10:29 PM
+ * Author : fedea
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
